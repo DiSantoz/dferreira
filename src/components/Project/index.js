7489e4e7230962755f4f218d0e64e41ed6227017
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-08-29 11:02:35 -0400

    styled portoflio tab, add tools and resize images and cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,17 +10,44 @@ function Project() {
       <div className="row row-cols-1 row-cols-md-2 g-4">
         {projectList.map((project, index) => (
           <div className="col">
-            <div className="card" style={{ width: "30rem" }} key={index}>
+            <div className="card h-100" style={{ width: "30rem" }} key={index}>
               <h3>{project.title}</h3>
-              <img src={project.image} className="card-img-top" alt="..."></img>
+              <img
+                src={project.image}
+                style={{
+                  width: "478px",
+                  height: "200px",
+                }}
+                className="card-img-top"
+                alt="..."
+              ></img>
               <p className="card-title">{project.category}</p>
               <p className="card-text">{project.description}</p>
-              <a href={project.deployed} className="btn btn-outline-primary">
-                <i class="fad fa-browser fa-2x"></i>
-              </a>
-              <a href={project.github} className="btn btn-outline-primary">
-                <i class="fab fa-github-square fa-2x"></i>
-              </a>
+              <div className="card-body">
+                <a href={project.deployed} className="btn btn-outline-primary">
+                  <i class="fad fa-browser fa-2x"></i>
+                </a>
+                <a href={project.github} className="btn btn-outline-primary">
+                  <i class="fab fa-github-square fa-2x"></i>
+                </a>
+              </div>
+              <div className="card-body">
+                <div className="card-footer badge rounded-pill bg-success">
+                  {project.tools1}{" "}
+                </div>
+                <div className="card-footer badge rounded-pill bg-primary">
+                  {project.tools2}
+                </div>
+                <div className="card-footer badge rounded-pill bg-danger">
+                  {project.tools3}
+                </div>
+                <div className="card-footer badge rounded-pill bg-warning">
+                  {project.tools4}
+                </div>
+                <div className="card-footer badge rounded-pill bg-info">
+                  {project.tools5}
+                </div>
+              </div>
             </div>
           </div>
         ))}
